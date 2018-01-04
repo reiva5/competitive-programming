98a5004d4e78bee33a0127c989924eff4a49a68f
reiva5 <jehiannormansaviero@gmail.com>
2018-01-04 17:42:33 +0700

    Add submission for open kattis 05-01-2018

---
+++ Open Kattis/parking.cpp
@@ -0,0 +1,83 @@
+/* Bismillahirrahmanirrahim */
+/* Author : Jehian Norman Saviero (@Reiva5) */
+#include <bits/stdc++.h>
+
+/* MACROS SAMPAH */
+#define Jehian using
+#define Norman namespace
+#define Saviero std
+#define cepet {ios_base::sync_with_stdio(0);cin.tie(0);}
+
+Jehian Norman Saviero;
+
+/* MACROS FOR TYPE */
+typedef long l;
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long ul;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<l> vl;
+typedef vector<ll> vll;
+typedef pair<int, int> pi;
+typedef pair<l,l> pl;
+typedef pair<ll,ll> pll;
+typedef pair<ld,ld> pld;
+
+/* MACROS FOR PRINT */
+#define nl printf("\n")
+#define sp printf(" ")
+
+/* MACROS FOR FUNCTION AND METHOD */
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define mp make_pair
+#define eb emplace_back
+#define RESET(x) memset((x), 0, sizeof(x))
+#define SET(x,y) for(int _je = sizeof(x)/sizeof(x[0]); _je--;) x[_je] = y
+#define SQR(x) (x)*(x)
+#define CUBE(x) SQR(x)*(x)
+#define ALL(x) (x).begin(), (x).end()
+#define ALLSIZE(x,n) (x), (x)+(n)
+
+/* MACROS FOR ATTRIBUTE */
+#define fi first
+#define se second
+
+/* MACROS FOR SCAN */
+#define scc(x) scanf("%c", &x)
+#define scd(x) scanf("%Lf", &x)
+#define scf(x) scanf("%f", &x)
+#define sci(x) scanf("%d", &x)
+#define scl(x) scanf("%ld", &x)
+#define scll(x) scanf("%lld", &x)
+#define scld(x) scanf("%Lf", &x)
+#define scui(x) sci(x)
+#define scul(x) scl(x)
+#define scull(x) scll(x)
+
+int main(){
+	ll bayar[4];
+	RESET(bayar);
+	for (ll i = 1; i <= 3; ++i){
+		scll(bayar[i]);
+	}
+	pll interval[3];
+	for (ll i = 0; i < 3; ++i){
+		scll(interval[i].fi);
+		scll(interval[i].se);
+	}
+	ll pay = 0;
+	ll truck;
+	for (ll i = 0; i < 100; ++i){
+		truck = 0;
+		for (ll j = 0; j < 3; ++j){
+			truck += interval[j].fi <= i && i < interval[j].se;
+		}
+		pay += bayar[truck]*truck;
+	}
+	cout << pay << endl;
+	return 0;
+}
