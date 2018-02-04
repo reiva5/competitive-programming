5eb6d4b9218fb59aeb302b81694e6969974f0951
reiva5 <jehiannormansaviero@gmail.com>
2018-02-04 18:01:13 +0700

    Add AtCoder Petrozavodsk Contest 001

---
+++ AtCoder/AtCoder Petrozavodsk Contest 001/apc001_b.cpp
@@ -0,0 +1,87 @@
+/* Bismillahirrahmanirrahim */
+/* Author : Jehian Norman Saviero (@Reiva5) */
+#include <bits/stdc++.h>
+
+/* MACROS SAMPAH */
+#define Jehian using
+#define Norman namespace
+#define Saviero std
+#define cepet {ios_base::sync_with_stdio(0);cin.tie(0);}
+
+Jehian Norman Saviero;
+
+/* MACROS FOR TYPE */
+typedef long l;
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long ul;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<l> vl;
+typedef vector<ll> vll;
+typedef pair<int, int> pi;
+typedef pair<l,l> pl;
+typedef pair<ll,ll> pll;
+typedef pair<ld,ld> pld;
+
+/* MACROS FOR PRINT */
+#define nl printf("\n")
+#define sp printf(" ")
+
+/* MACROS FOR FUNCTION AND METHOD */
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define mp make_pair
+#define eb emplace_back
+#define RESET(x) memset((x), 0, sizeof(x))
+#define SET(x,y) for(int _je = sizeof(x)/sizeof(x[0]); _je--;) x[_je] = y
+#define SQR(x) (x)*(x)
+#define CUBE(x) SQR(x)*(x)
+#define ALL(x) (x).begin(), (x).end()
+#define ALLSIZE(x,n) (x), (x)+(n)
+
+/* MACROS FOR ATTRIBUTE */
+#define fi first
+#define se second
+
+/* MACROS FOR SCAN */
+#define scc(x) scanf("%c", &x)
+#define scd(x) scanf("%Lf", &x)
+#define scf(x) scanf("%f", &x)
+#define sci(x) scanf("%d", &x)
+#define scl(x) scanf("%ld", &x)
+#define scll(x) scanf("%lld", &x)
+#define scld(x) scanf("%Lf", &x)
+#define scui(x) sci(x)
+#define scul(x) scl(x)
+#define scull(x) scll(x)
+
+int main(){
+	ll N;
+	scll(N);
+	vll data[2];
+	for (ll i = 0; i < 2; ++i){
+		ll tmp;
+		for (ll j = 0; j < N; ++j){
+			scll(tmp);
+			data[i].pb(tmp);
+		}
+	}
+	ll par[2];
+	RESET(par);
+	for (ll i = 0; i < N; ++i){
+		if (data[0][i] > data[1][i]){
+			par[1] += data[0][i]-data[1][i];
+		} else {
+			par[0] += (data[1][i]-data[0][i])/2;
+		}
+	}
+	if (par[1] > par[0]){
+		puts("No");
+	} else {
+		puts("Yes");
+	}
+	return 0;
+}
